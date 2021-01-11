0f977930d05cf594ec7e9e0b3f40d19dc19e5ec9
Arun Jeevanantham <itsmearun98@gmail.com>
2021-01-11 16:48:49 -0500

    basic theming and setup splash page

---
+++ components/Splash.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Splash() {
+    return (
+        <div className="bg-secondary h-vh90">
+            <div className="p-5 text-5xl flex-row"> 
+                <div className="text-secondary"> Hi, I'm </div>
+                <div className="text-accent"> Arun </div>
+            </div>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: {
+      'primary': '#F1F0EA',
+      'secondary': '#2D232E',
+    },
+    textColor: {
+      'primary': '#2D232E',
+      'secondary': '#E0DDCF',
+      'accent': '#AF9FA5'
+    },
+    height: {
+      vh90: '90vh'
+    },
     extend: {},
   },
   variants: {

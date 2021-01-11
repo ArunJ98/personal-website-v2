e5d78a696eb890389d5a76a08f254dd806057ef1
Arun Jeevanantham <itsmearun98@gmail.com>
2021-01-11 18:48:55 -0500

    flexbox intro

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Splash() {
     return (
         <div className="bg-secondary h-vh90">
-            <div className="p-5 text-5xl flex-row"> 
-                <div className="text-secondary"> Hi, I'm </div>
-                <div className="text-accent"> Arun </div>
+            <div className="p-5 text-5xl flex flex-row"> 
+                <p className="text-secondary"> Hi, I'm&#160; </p>
+                <p className="text-accent"> Arun. </p>
             </div>
         </div>
     )
